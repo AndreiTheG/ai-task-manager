6dd718ebe2e6af3f6a7e14b4dd99530df193331c
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2026-05-02 13:51:15 +0300

    add route for tasks

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,15 +1,25 @@
 // /api/tasks/route.js
-
 import { NextResponse } from 'next/server';
 import { getCollection } from '@/lib/mongodb';
+import { getServerSession } from 'next-auth';
 
 export async function GET() {
+    const session = await getServerSession();
+    if (!session) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
     const tasks = await getCollection('tasks');
-    const all = await tasks.find({}).toArray();
+    const all = await tasks.find({ userId: session.user.email }).toArray();
     return NextResponse.json(all);
 }
 
 export async function POST(request) {
+    const session = await getServerSession();
+    if (!session) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
     const body = await request.json();
     const tasks = await getCollection('tasks');
     const { insertedId } = await tasks.insertOne(body);
